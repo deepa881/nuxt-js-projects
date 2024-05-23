57cca834882b5ecefd515a78b6f8554c04078f89
Todime Deepa <122363964+deepa881@users.noreply.github.com>
2024-05-23 08:09:08 +0530

    Add files via upload

---
+++ Menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav >
+    <ul class="flex space-x-4">
+      <li>
+        <NuxtLink to="/" class="link">Main</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about" class="link">About</NuxtLink>
+      </li>
+       <li>
+        <NuxtLink to="/projects" class="link">Projects</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.link {
+  @apply p-2 hover:bg-gray-300
+}
+</style>
